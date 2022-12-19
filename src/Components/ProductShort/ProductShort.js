6419f0bbeb7b15d2ef65a27a3916a10d6f23e662
SOURAVMONDAL-BD <souravmondalcode@gmail.com>
2022-12-19 17:47:21 +0600

    product card change

--- src/Components/ProductShort/ProductShort.js
+++ src/Components/ProductShort/ProductShort.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const ProductShort = () => {
+const ProductShort = ({ products }) => {
     return (
-        <div>
-            <h1>product short details</h1>
+        <div className="card card-compact w-64 bg-base-100 shadow-xl mx-auto">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="products" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{products?.productName}</h2>
+                <div className='flex justify-between'>
+                    <div>
+                        <p>{products?.postingDate}</p>
+                        <p>{products?.location}</p>
+                    </div>
+                    <div>
+                        <p className='text-2xl font-semibold text-red-600'>$ {products.resalePrice}</p>
+                    </div>
+                </div>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-sm bg-gradient-to-r from-cyan-500 to-teal-500 border-none">Details</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/CategoriesHome/CategoriesHome.js
+++ src/Pages/Home/CategoriesHome/CategoriesHome.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from '../../../Components/ProductCard/ProductCard';
+import ProductShort from '../../../Components/ProductShort/ProductShort';
 import { AuthContext } from '../../../Context/AurhProvider/AuthProvider';
 import CategoriesCard from './CategoriesCard';
 
@@ -62,12 +63,12 @@ const CategoriesHome = () => {
                 {
                     categoryProducts ?
                         <>
-                            <div className=' lg:mx-20 grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto my-10'>
+                            <div className=' lg:mx-20 grid grid-cols-1 md:grid-cols-3 gap-10 mx-auto my-10'>
                                 {
-                                    categoryProducts.map(products => <ProductCard
+                                    categoryProducts.map(products => <ProductShort
                                         key={products._id}
                                         products={products}
-                                    ></ProductCard>)
+                                    ></ProductShort>)
                                 }
 
                             </div>
